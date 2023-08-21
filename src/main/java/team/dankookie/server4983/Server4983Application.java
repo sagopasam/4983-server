ae2b663d8d9724e1aeaf00697de1ad5f253454fa
wan2daaa <wan2daaa@gmail.com>
2023-08-21 17:49:38 +0900

    init : 프로젝트 초기화

---
+++ src/main/java/team/dankookie/server4983/Server4983Application.java
@@ -0,0 +1,13 @@
+package team.dankookie.server4983;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Server4983Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Server4983Application.class, args);
+    }
+
+}
---
+++ src/test/java/team/dankookie/server4983/Server4983ApplicationTests.java
@@ -0,0 +1,13 @@
+package team.dankookie.server4983;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Server4983ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
