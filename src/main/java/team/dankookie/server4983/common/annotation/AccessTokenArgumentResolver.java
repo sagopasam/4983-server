2f69d567d826c46a4c7ad0190562201fb707d77f
wan2daaa <wan2daaa@gmail.com>
2023-08-21 17:52:43 +0900

    feat: header에 존재하는 accessToken을 처리하는 어노테이션 구현

---
+++ src/main/java/team/dankookie/server4983/common/annotation/AccessTokenArgumentResolver.java
@@ -0,0 +1,38 @@
+package team.dankookie.server4983.common.annotation;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+@RequiredArgsConstructor
+public class AccessTokenArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean hasLoginAnnotation = parameter.hasParameterAnnotation(AccessToken.class);
+        boolean hasUserType = AccessToken.class.isAssignableFrom(parameter.getParameterType());
+
+        return hasLoginAnnotation && hasUserType;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+
+        String authorizationHeader = request.getHeader("Authorization");
+        return getAccessToken(authorizationHeader);
+    }
+
+    private String getAccessToken(String authorizationHeader) {
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            return authorizationHeader.substring(7);
+        }
+        return null;
+    }
+}
