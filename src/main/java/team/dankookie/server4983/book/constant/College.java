c48d1b2761c10f9e4aa4f38d9ab0499ddc34fa09
wan2daaa <wan2daaa@gmail.com>
2023-08-24 19:07:09 +0900

    feat: 엔티티에 필요한 enum 구현

---
+++ src/main/java/team/dankookie/server4983/book/constant/College.java
@@ -0,0 +1,19 @@
+package team.dankookie.server4983.book.constant;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum College {
+    BUSINESS_AND_ECONOMICS("경영경제대학" ),
+    SW_CONVERGENCE("SW융합대학"),
+    SOCIAL_SCIENCES("사회과학대학"),
+    LITERAL_ARTS("문과대학"),
+    LAW("법과대학"),
+    ENGINEERING("공과대학"),
+    EDUCATION("사범대학"),
+    MUSIC_AND_ARTS("음악예술대학");
+
+    private final String koName;
+}
