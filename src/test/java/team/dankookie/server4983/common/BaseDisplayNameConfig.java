9dcf08956acde64b51196b5a600939a04bb3027a
wan2daaa <wan2daaa@gmail.com>
2023-08-30 18:12:56 +0900

    feat: 테스트시 사용할 공통 클래스 생성 (controller, service, repository)

---
+++ src/test/java/team/dankookie/server4983/common/BaseDisplayNameConfig.java
@@ -0,0 +1,9 @@
+package team.dankookie.server4983.common;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public abstract class BaseDisplayNameConfig {
+}
