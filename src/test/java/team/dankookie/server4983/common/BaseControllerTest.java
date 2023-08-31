efa4f4007069185d7868c64014e8b45ca8e7c2c5
wan2daaa <wan2daaa@gmail.com>
2023-08-31 18:46:58 +0900

    refactor: BaseControllerTest 필요없는 어노테이션 제거 및 추가

--- src/test/java/team/dankookie/server4983/common/BaseControllerTest.java
+++ src/test/java/team/dankookie/server4983/common/BaseControllerTest.java
@@ -2,18 +2,14 @@ package team.dankookie.server4983.common;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -23,11 +19,11 @@ import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.docu
 
 @SpringBootTest
 @AutoConfigureRestDocs
+@AutoConfigureMockMvc
 @ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
-@MockBean(JpaMetamodelMappingContext.class)
-@WebAppConfiguration
 public abstract class BaseControllerTest extends BaseDisplayNameConfig {
 
+    @Autowired
     protected MockMvc mockMvc;
 
     @Autowired
