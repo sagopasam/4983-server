dddd83f4cc84bafd469f56aa2bf2a69d0be0281a
wan2daaa <wan2daaa@gmail.com>
2023-09-11 02:41:09 +0900

    feat: AccessToken 클래스로 accessToken 추출하는 로직 구현

--- src/main/java/team/dankookie/server4983/Server4983Application.java
+++ src/main/java/team/dankookie/server4983/Server4983Application.java
@@ -2,7 +2,10 @@ package team.dankookie.server4983;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import team.dankookie.server4983.jwt.constants.TokenSecretKey;
 
+@EnableConfigurationProperties(TokenSecretKey.class)
 @SpringBootApplication
 public class Server4983Application {
 
---
+++ src/main/java/team/dankookie/server4983/common/config/WebMvcConfig.java
@@ -0,0 +1,23 @@
+package team.dankookie.server4983.common.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import team.dankookie.server4983.jwt.constants.TokenSecretKey;
+import team.dankookie.server4983.jwt.resolver.LoginResolver;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final TokenSecretKey tokenSecretKey;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginResolver(tokenSecretKey));
+
+    }
+}
