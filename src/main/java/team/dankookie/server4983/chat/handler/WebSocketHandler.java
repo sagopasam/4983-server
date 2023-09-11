eea7add44a8c9b59a84be6c13713e72f139488c1
5tr1ker <tjseocld@naver.com>
2023-09-12 02:39:46 +0900

    fix: 소캣 핸들러 수정
    fix: 소캣 핸들러 수정

--- src/main/java/team/dankookie/server4983/chat/handler/WebSocketHandler.java
+++ src/main/java/team/dankookie/server4983/chat/handler/WebSocketHandler.java
@@ -1,6 +1,7 @@
 package team.dankookie.server4983.chat.handler;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.websocket.Session;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -19,26 +20,32 @@ import static team.dankookie.server4983.chat.dto.ChatResponse.createChatResponse
 @Component
 @RequiredArgsConstructor
 public class WebSocketHandler extends TextWebSocketHandler {
-    
+
     private Map<String, WebSocketSession> sessionMap = new HashMap<>();
+    private final ChatLogicHandler chatLogicHandler;
+    ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        ObjectMapper objectMapper = new ObjectMapper();
         ChatRequest chatMessage = objectMapper.readValue(message.getPayload(), ChatRequest.class);
 
-        sendData(objectMapper , chatMessage);
+        System.out.println("받은 데이터 Type : " + chatMessage.getContentType());
 
+        chatLogicHandler.chatLoginHandler(chatMessage , chatMessage.getData());
+        sendData(chatMessage);
     }
 
-    private void sendData(ObjectMapper objectMapper, ChatRequest chatMessage) throws IOException {
-        ChatResponse chatResponse = createChatResponse(chatMessage);
+    private void sendData(ChatRequest chatMessage) throws IOException {
+        ChatResponse chatResponse = createChatResponse((String) chatMessage.getData().get("message") , chatMessage);
         String result = objectMapper.writeValueAsString(chatResponse);
 
-        WebSocketSession receiver = sessionMap.get("세션 아이디를 넣어주세요.");
+        WebSocketSession receiver = sessionMap.get(chatMessage.getSession());
 
         if(receiver != null && receiver.isOpen()) {
             receiver.sendMessage(new TextMessage(result));
+            System.out.println("메세지 전송 : " + chatMessage.getData().get("message"));
+        } else {
+            System.out.println("찾을 수 없는 사용자 : " + chatMessage.getSession());
         }
     }
 
@@ -48,6 +55,8 @@ public class WebSocketHandler extends TextWebSocketHandler {
         sessionMap.put(sessionId, session);
 
         System.out.println("세션 시작 : " + session.getId());
+        String result = objectMapper.writeValueAsString(createChatResponse("성공적으로 연결되었습니다." , session.getId()));
+        session.sendMessage(new TextMessage(result));
 
         super.afterConnectionEstablished(session);
     }
