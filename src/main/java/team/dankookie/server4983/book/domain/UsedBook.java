5014dc39ffcfc7624686a3e1a34be4fdd8d4faf0
5tr1ker <tjseocld@naver.com>
2023-09-20 03:14:57 +0900

    fix: 테스트 데이터 코드 수정
    fix: 테스트 데이터 코드 수정

--- src/main/java/team/dankookie/server4983/book/domain/UsedBook.java
+++ src/main/java/team/dankookie/server4983/book/domain/UsedBook.java
@@ -61,8 +61,22 @@ public class UsedBook extends BaseEntity {
     @Column(columnDefinition = "boolean default false")
     private Boolean isDeleted;
 
+    // FIXME 테스트 용 USEDBOOK 생성자 ( 테스트 데이터가 필요 없어진게 아니면 절대 수정하지 마세요!! )
     public UsedBook(long id, String bookName, int price, LocalDate now, String publisher, College college, Department department, BookStatus bookStatus, boolean isUnderlinedOrWrite, boolean isDiscolorationAndDamage, boolean isCoverDamaged, Member buyer, Member seller) {
-        super();
+        this.id = id;
+        this.name = bookName;
+        this.price = price;
+        this.tradeAvailableDate = now;
+        this.publisher = publisher;
+        this.college = college;
+        this.department = department;
+        this.bookStatus = bookStatus;
+        this.isUnderlinedOrWrite = isUnderlinedOrWrite;
+        this.isDiscolorationAndDamage = isDiscolorationAndDamage;
+        this.isCoverDamaged = isCoverDamaged;
+        this.buyerMember = buyer;
+        this.sellerMember = seller;
+        this.isDeleted = false;
     }
 
     public void setIsDeletedTrue() {
