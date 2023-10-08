34c562527d94b53603023f59f011019ccb4d1468
5tr1ker <tjseocld@naver.com>
2023-10-08 18:00:42 +0900

    feat: 운영자 연락 로직 구현
    feat: 운영자 연락 로직 구현

--- src/main/java/team/dankookie/server4983/scheduler/repository/CustomSchedulerRepository.java
+++ src/main/java/team/dankookie/server4983/scheduler/repository/CustomSchedulerRepository.java
@@ -1,5 +1,6 @@
 package team.dankookie.server4983.scheduler.repository;
 
+import team.dankookie.server4983.chat.domain.ChatRoom;
 import team.dankookie.server4983.scheduler.entity.Schedule;
 
 import java.time.LocalDateTime;
@@ -8,5 +9,9 @@ import java.util.List;
 public interface CustomSchedulerRepository {
 
     List<Schedule> findByAlertTime(LocalDateTime time);
+
+    List<ChatRoom> findChatRoomPreviouslyTime(long time , int interactStep);
+
+    List<ChatRoom> findChatRoomAfterTime(long time , int interactStep);
 
 }
--- src/main/java/team/dankookie/server4983/scheduler/repository/SchedulerRepositoryImpl.java
+++ src/main/java/team/dankookie/server4983/scheduler/repository/SchedulerRepositoryImpl.java
@@ -2,11 +2,15 @@ package team.dankookie.server4983.scheduler.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import team.dankookie.server4983.chat.constant.ContentType;
+import team.dankookie.server4983.chat.domain.ChatRoom;
 import team.dankookie.server4983.scheduler.entity.Schedule;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+import static team.dankookie.server4983.book.domain.QUsedBook.usedBook;
+import static team.dankookie.server4983.chat.domain.QChatRoom.chatRoom;
 import static team.dankookie.server4983.scheduler.entity.QSchedule.schedule;
 
 @RequiredArgsConstructor
@@ -22,6 +26,30 @@ public class SchedulerRepositoryImpl implements CustomSchedulerRepository {
         return jpaQueryFactory.select(schedule)
                 .from(schedule)
                 .where(schedule.time.goe(start).and(schedule.time.loe(end)))
+                .fetch();
+    }
+
+    @Override
+    public List<ChatRoom> findChatRoomPreviouslyTime(long time , int interactStep) {
+        LocalDateTime start = LocalDateTime.now().minusMinutes(time);
+        LocalDateTime end = LocalDateTime.now();
+
+        return jpaQueryFactory.select(chatRoom).from(chatRoom)
+                .innerJoin(chatRoom.usedBook , usedBook)
+                .on(usedBook.tradeAvailableDatetime.goe(start).and(usedBook.tradeAvailableDatetime.loe(end)))
+                .where(chatRoom.interactStep.eq(interactStep))
+                .fetch();
+    }
+
+    @Override
+    public List<ChatRoom> findChatRoomAfterTime(long time , int interactStep) {
+        LocalDateTime start = LocalDateTime.now();
+        LocalDateTime end = LocalDateTime.now().plusMinutes(time);
+
+        return jpaQueryFactory.select(chatRoom).from(chatRoom)
+                .innerJoin(chatRoom.usedBook , usedBook)
+                .on(usedBook.tradeAvailableDatetime.goe(start).and(usedBook.tradeAvailableDatetime.loe(end)))
+                .where(chatRoom.interactStep.eq(interactStep))
                 .fetch();
     }
 
