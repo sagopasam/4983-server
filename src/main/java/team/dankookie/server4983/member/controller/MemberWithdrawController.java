dcf8c0170c0ca4e4af920db669a2782fe2c76877
MJ <rlaalswls102@naver.com>
2023-10-16 01:06:43 +0900

    fix: 마이페이지 회원탈퇴 docu수정 및 refreshToken 추가

--- src/main/java/team/dankookie/server4983/member/controller/MemberWithdrawController.java
+++ src/main/java/team/dankookie/server4983/member/controller/MemberWithdrawController.java
@@ -1,8 +1,11 @@
 package team.dankookie.server4983.member.controller;
 
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,12 +19,16 @@ public class MemberWithdrawController {
     private final MemberService memberService;
 
     @PatchMapping("/withdraw")
-    public ResponseEntity<String> withdraw(AccessToken accessToken) {
+    public ResponseEntity<String> withdraw(HttpServletResponse response, AccessToken accessToken, @CookieValue(name = "refreshToken", required = false) Cookie refreshTokenCookie) {
         boolean isWithdraw = memberService.checkMemberAndWithdraw(accessToken);
         if (isWithdraw) {
-            return ResponseEntity.ok("회원 탈퇴가 완료되었습니다.");
-        } else {
-            return ResponseEntity.badRequest().body("이미 탈퇴한 회원이거나 회원을 찾을 수 없습니다.");
+                refreshTokenCookie.setMaxAge(0);
+                response.addCookie(refreshTokenCookie);
+                return ResponseEntity.ok("회원 탈퇴가 완료되었습니다.");
+
+        }else {
+                return ResponseEntity.badRequest().body("이미 탈퇴한 회원이거나 회원을 찾을 수 없습니다.");
+            }
         }
     }
-}
+
