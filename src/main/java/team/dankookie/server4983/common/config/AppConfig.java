4e90f3437a3b6895641b63bb1db2875b106757cd
wan2daaa <wan2daaa@gmail.com>
2023-10-16 17:51:47 +0900

    feat: @Async 사용하기 위해 설정

---
+++ src/main/java/team/dankookie/server4983/common/config/AppConfig.java
@@ -0,0 +1,26 @@
+package team.dankookie.server4983.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+public class AppConfig {
+
+  @Bean(name = "defaultTaskExecutor", destroyMethod = "shutdown")
+  public ThreadPoolTaskExecutor defaultTaskExecutor() {
+    ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+    executor.setCorePoolSize(200);
+    executor.setMaxPoolSize(300);
+    return executor;
+  }
+
+  @Bean(name = "messagingTaskExecutor", destroyMethod = "shutdown")
+  public ThreadPoolTaskExecutor messagingTaskExecutor() {
+    ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+    executor.setCorePoolSize(50);
+    executor.setMaxPoolSize(100);
+    return executor;
+  }
+
+}
