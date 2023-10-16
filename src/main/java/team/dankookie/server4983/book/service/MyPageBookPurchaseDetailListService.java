8a81a796f04e42fcda577307bdebcb43e5d86d6a
MJ <rlaalswls102@naver.com>
2023-10-17 00:33:53 +0900

    fix: 판매내역, 구매내역 Service 수정

--- src/main/java/team/dankookie/server4983/book/service/MyPageBookPurchaseDetailListService.java
+++ src/main/java/team/dankookie/server4983/book/service/MyPageBookPurchaseDetailListService.java
@@ -5,9 +5,7 @@ import org.springframework.stereotype.Service;
 import team.dankookie.server4983.book.constant.BookStatus;
 import team.dankookie.server4983.book.dto.UsedBookListResponse;
 import team.dankookie.server4983.book.repository.mypageBookPurchaseDetail.BookPurchaseDetailRepository;
-import team.dankookie.server4983.jwt.constants.TokenSecretKey;
 import team.dankookie.server4983.jwt.dto.AccessToken;
-import team.dankookie.server4983.jwt.util.JwtTokenUtils;
 import team.dankookie.server4983.member.domain.Member;
 import team.dankookie.server4983.member.repository.MemberRepository;
 
@@ -20,16 +18,13 @@ import java.util.List;
 public class MyPageBookPurchaseDetailListService {
     private final BookPurchaseDetailRepository bookPurchaseDetailRepository;
     private final MemberRepository memberRepository;
-    private final JwtTokenUtils jwtTokenUtils;
-    private final TokenSecretKey tokenSecretKey;
 
     public List<UsedBookListResponse> getMyPageBookPurchaseDetailList(BookStatus bookStatus, AccessToken accessToken){
 
-        String nickname = jwtTokenUtils.getNickname(accessToken.value(), tokenSecretKey.getSecretKey());
+        String nickname = accessToken.nickname();
 
         Member member = memberRepository.findByNickname(nickname)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원입니다."));
-
 
     return bookPurchaseDetailRepository.getMyPageBookPurchaseDetailList(bookStatus, member.getId());
     }
