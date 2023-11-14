0024685583e1c99d3d3e8d61b925026716f32f8e
MJ <rlaalswls102@naver.com>
2023-11-15 01:44:00 +0900

    feat: 마이페이지 수정하기 들어갈때 비밀번호 확인 api 구현

--- src/main/java/team/dankookie/server4983/member/controller/MemberMyPageController.java
+++ src/main/java/team/dankookie/server4983/member/controller/MemberMyPageController.java
@@ -1,14 +1,15 @@
 package team.dankookie.server4983.member.controller;
 
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import team.dankookie.server4983.jwt.dto.AccessToken;
 import team.dankookie.server4983.member.dto.MemberMyPageModifyResponse;
 import team.dankookie.server4983.member.dto.MemberMyPageResponse;
+import team.dankookie.server4983.member.dto.MemberPasswordMatchResponse;
 import team.dankookie.server4983.member.service.MemberService;
 
 @RequiredArgsConstructor
@@ -23,6 +24,12 @@ public class MemberMyPageController {
     MemberMyPageResponse response = memberService.getMyPageMemberInfo(
         accessToken.nickname());
     return ResponseEntity.ok(response);
+  }
+
+  @PostMapping("/password")
+  public ResponseEntity<MemberPasswordMatchResponse> checkPasswordMatch(@RequestBody String password, HttpServletRequest request){
+    boolean isPasswordMatch = memberService.isMemberPasswordMatch(password, request.getHeader(HttpHeaders.AUTHORIZATION));
+    return ResponseEntity.ok(MemberPasswordMatchResponse.of(isPasswordMatch));
   }
 
   @GetMapping("/modify")
