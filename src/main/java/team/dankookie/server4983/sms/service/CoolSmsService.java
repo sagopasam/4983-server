97639ed20cf76604117e558741e489b3c097e3d4
wan2daaa <wan2daaa@gmail.com>
2024-01-30 20:01:11 +0900

    fix: 메세지 잔액 부족시 에러 안나오게 수정

--- src/main/java/team/dankookie/server4983/sms/service/CoolSmsService.java
+++ src/main/java/team/dankookie/server4983/sms/service/CoolSmsService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 import javax.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.nurigo.sdk.NurigoApp;
 import net.nurigo.sdk.message.model.Message;
 import net.nurigo.sdk.message.request.SingleMessageSendingRequest;
@@ -13,6 +14,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import team.dankookie.server4983.sms.dto.SmsCertificationNumber;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class CoolSmsService {
@@ -34,42 +36,59 @@ public class CoolSmsService {
   }
 
   public SingleMessageSentResponse sendMessage(String to, String content) {
-    Message message = new Message();
+    try {
 
-    message.setFrom(senderPhone);
-    message.setTo(to);
-    message.setText(content);
+      Message message = new Message();
 
-    return this.messageService.sendOne(new SingleMessageSendingRequest(message));
+      message.setFrom(senderPhone);
+      message.setTo(to);
+      message.setText(content);
+
+      return this.messageService.sendOne(new SingleMessageSendingRequest(message));
+    } catch (Exception e) {
+      log.warn("SMS 전송 실패, {}", e.getMessage());
+      return null;
+    }
   }
 
 
   public SmsCertificationNumber sendCertificationNumberToPhoneNumber(String phoneNumber) {
-    Message message = new Message();
+    try {
 
-    int randomNumber = ThreadLocalRandom.current().nextInt(100000, 1000000);
-    String content = "안녕하세요! 4983 입니다. \n인증번호는 " + randomNumber + " 입니다.";
+      Message message = new Message();
 
-    message.setFrom(senderPhone);
-    message.setTo(phoneNumber);
-    message.setText(content);
+      int randomNumber = ThreadLocalRandom.current().nextInt(100000, 1000000);
+      String content = "안녕하세요! 4983 입니다. \n인증번호는 " + randomNumber + " 입니다.";
 
-    this.messageService.sendOne(new SingleMessageSendingRequest(message));
+      message.setFrom(senderPhone);
+      message.setTo(phoneNumber);
+      message.setText(content);
 
-    return SmsCertificationNumber.of(String.valueOf(randomNumber));
+      this.messageService.sendOne(new SingleMessageSendingRequest(message));
+
+      return SmsCertificationNumber.of(String.valueOf(randomNumber));
+    }catch (Exception e) {
+      log.warn("SMS 전송 실패, {}", e.getMessage());
+      return SmsCertificationNumber.of("000000");
+    }
   }
 
 
   public void sendAdminToSms(String content) {
-    List<String> adminPhoneNumberList = List.of("01044873122", "01096100950", "01073730246",
-        "01032910525");
+    try {
+      List<String> adminPhoneNumberList = List.of("01044873122", "01096100950", "01073730246",
+          "01032910525");
 
       Message message = new Message();
-    for (String phoneNumber : adminPhoneNumberList) {
-      message.setFrom(senderPhone);
-      message.setTo(phoneNumber);
-      message.setText(content);
-      this.messageService.sendOne(new SingleMessageSendingRequest(message));
+      for (String phoneNumber : adminPhoneNumberList) {
+        message.setFrom(senderPhone);
+        message.setTo(phoneNumber);
+        message.setText(content);
+        this.messageService.sendOne(new SingleMessageSendingRequest(message));
+      }
+    } catch (Exception e) {
+      log.warn("SMS 전송 실패, {}", e.getMessage());
     }
+
   }
 }
