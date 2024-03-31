fb6b0346f27c3b7e8509e72cfbe393380e960b8e
suyong choi <dpfprtus@gmail.com>
2024-03-31 16:52:51 +0900

    [YTFU-20] id 값에 의존하던 테스트 방식을 수정

--- src/test/java/team/dankookie/server4983/chat/service/AdminChatServiceTest.java
+++ src/test/java/team/dankookie/server4983/chat/service/AdminChatServiceTest.java
@@ -6,10 +6,7 @@ import static org.mockito.Mockito.doNothing;
 
 import java.time.LocalDateTime;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +18,7 @@ import team.dankookie.server4983.book.domain.UsedBook;
 import team.dankookie.server4983.book.repository.usedBook.UsedBookRepository;
 import team.dankookie.server4983.chat.domain.ChatRoom;
 import team.dankookie.server4983.chat.dto.ChatRoomRequest;
+import team.dankookie.server4983.chat.dto.ChatRoomResponse;
 import team.dankookie.server4983.chat.exception.ChatException;
 import team.dankookie.server4983.chat.repository.ChatRoomRepository;
 import team.dankookie.server4983.fcm.service.FcmService;
@@ -31,7 +29,6 @@ import team.dankookie.server4983.member.repository.MemberRepository;
 
 @SpringBootTest
 @Transactional
-@TestMethodOrder(OrderAnnotation.class)
 @ExtendWith(MockitoExtension.class)
 public class AdminChatServiceTest {
 
@@ -54,17 +51,13 @@ public class AdminChatServiceTest {
     FcmService fcmService;
 
     @Test
-    @Order(1)
     @DisplayName("관리자가 판매자에게 메세지를 보낸다.")
-    void postChattingToSeller() {
+    public void postChattingToSeller() {
 
         //given
-        final long chatRoomId = 1L;
-        final long usedBookId = 1L;
         final String buyerId = "buyer";
         final String sellerId = "seller";
         final LocalDateTime now = LocalDateTime.now();
-        final ChatRoomRequest chatRoomRequest = ChatRoomRequest.of(usedBookId);
 
         Member buyer = MemberFixture.createMemberByStudentId(buyerId);
         Member seller = MemberFixture.createMemberByStudentIdAndNickname(sellerId,
@@ -72,42 +65,32 @@ public class AdminChatServiceTest {
 
         memberRepository.save(buyer);
         memberRepository.save(seller);
+        UsedBook savedBook = usedBookRepository.save(createBook(buyer, seller, now));
 
-        UsedBook usedBook = UsedBook.builder()
-                .name("C언어")
-                .buyerMember(buyer)
-                .sellerMember(seller)
-                .bookStatus(BookStatus.SALE)
-                .price(100)
-                .tradeAvailableDatetime(now)
-                .build();
-        usedBookRepository.save(usedBook);
+        ChatRoomRequest chatRoomRequest = ChatRoomRequest.of(savedBook.getId());
 
         doNothing().when(fcmService).sendChattingNotificationByToken(any());
-        chatService.createChatRoom(chatRoomRequest, AccessToken.of("test", buyerId));
+        ChatRoomResponse savedChatRoom = chatService.createChatRoom(chatRoomRequest, AccessToken.of("test", buyerId));
 
         //when
-        adminChatService.postSellerChat(chatRoomId, "[관리자 메세지] 관리자 메세지가 왔습니다.");
+        adminChatService.postSellerChat(savedChatRoom.getChatRoomId(), "[관리자 메세지] 관리자 메세지가 왔습니다.");
 
         //then
-        ChatRoom chatRoom = chatRoomRepository.findByChatRoomId(chatRoomId)
+        ChatRoom chatRoom = chatRoomRepository.findByChatRoomId(savedChatRoom.getChatRoomId())
                 .orElseThrow(() -> new ChatException("채팅방을 찾을 수 없습니다."));
 
         assertThat(chatRoom.getSellerChats().get(2).getMessage()).isEqualTo("[관리자 메세지] 관리자 메세지가 왔습니다.");
     }
 
+
     @Test
-    @Order(2)
     @DisplayName("관리자가 구매자에게 메세지를 보낸다.")
-    void postChattingToBuyer() {
+    public void postChattingToBuyer() {
 
         //given
-        final long chatRoomId = 2L;
-        final long usedBookId = 2L;
         final String buyerId = "buyer";
         final String sellerId = "seller";
         final LocalDateTime now = LocalDateTime.now();
-        final ChatRoomRequest chatRoomRequest = ChatRoomRequest.of(usedBookId);
 
         Member buyer = MemberFixture.createMemberByStudentId(buyerId);
         Member seller = MemberFixture.createMemberByStudentIdAndNickname(sellerId,
@@ -115,8 +98,25 @@ public class AdminChatServiceTest {
 
         memberRepository.save(buyer);
         memberRepository.save(seller);
+        UsedBook savedBook = usedBookRepository.save(createBook(buyer, seller, now));
 
-        UsedBook usedBook = UsedBook.builder()
+        ChatRoomRequest chatRoomRequest = ChatRoomRequest.of(savedBook.getId());
+
+        doNothing().when(fcmService).sendChattingNotificationByToken(any());
+        ChatRoomResponse savedChatRoom = chatService.createChatRoom(chatRoomRequest, AccessToken.of("test", buyerId));
+
+        //when
+        adminChatService.postBuyerChat(savedChatRoom.getChatRoomId(), "[관리자 메세지] 관리자 메세지가 왔습니다.");
+
+        //then
+        ChatRoom chatRoom = chatRoomRepository.findByChatRoomId(savedChatRoom.getChatRoomId())
+                .orElseThrow(() -> new ChatException("채팅방을 찾을 수 없습니다."));
+
+        assertThat(chatRoom.getBuyerChats().get(2).getMessage()).isEqualTo("[관리자 메세지] 관리자 메세지가 왔습니다.");
+    }
+
+    private UsedBook createBook(Member buyer, Member seller, LocalDateTime now) {
+        return UsedBook.builder()
                 .name("C언어")
                 .buyerMember(buyer)
                 .sellerMember(seller)
@@ -124,20 +124,6 @@ public class AdminChatServiceTest {
                 .price(100)
                 .tradeAvailableDatetime(now)
                 .build();
-
-        usedBookRepository.save(usedBook);
-
-        doNothing().when(fcmService).sendChattingNotificationByToken(any());
-        chatService.createChatRoom(chatRoomRequest, AccessToken.of("test", buyerId));
-
-        //when
-        adminChatService.postBuyerChat(chatRoomId, "[관리자 메세지] 관리자 메세지가 왔습니다.");
-
-        //then
-        ChatRoom chatRoom = chatRoomRepository.findByChatRoomId(chatRoomId)
-                .orElseThrow(() -> new ChatException("채팅방을 찾을 수 없습니다."));
-
-        assertThat(chatRoom.getBuyerChats().get(2).getMessage()).isEqualTo("[관리자 메세지] 관리자 메세지가 왔습니다.");
     }
 }
 
