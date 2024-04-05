edf7bd983b9c164ef04ab97c949a24b5370197c3
suyong choi <dpfprtus@gmail.com>
2024-04-05 19:30:33 +0900

    [CD 테스트]

--- src/main/java/team/dankookie/server4983/book/domain/BookImage.java
+++ src/main/java/team/dankookie/server4983/book/domain/BookImage.java
@@ -1,6 +1,11 @@
 package team.dankookie.server4983.book.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,7 +16,8 @@ import lombok.NoArgsConstructor;
 @Entity
 public class BookImage {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     private Long id;
 
     private String imageUrl;
